2ce0de8362b4aa106e30cbe8683ef9984efd8672
Oleksandr <superbowl@ukr.net>
2022-10-15 19:32:42 +0300

    Adds header with styles

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,14 +1,24 @@
 import { Routes, Route } from 'react-router-dom';
 import GlobalStyles from 'GlobalStyles';
+import { Container, Header, Link } from './App.styled';
 
 export const App = () => {
   return (
-    <div>
+    <Container>
+      <Header>
+        <nav>
+          <Link to="/" end>
+            Home
+          </Link>
+          <Link to="/movies">Movies</Link>
+        </nav>
+      </Header>
       <Routes>
         <Route path="/" element={<div>Home</div>} />
+        <Route path="/movies" element={<div>Movies</div>} />
+        <Route path="*" element={<div>Error</div>} />
       </Routes>
       <GlobalStyles />
-      React homework template
-    </div>
+    </Container>
   );
 };
