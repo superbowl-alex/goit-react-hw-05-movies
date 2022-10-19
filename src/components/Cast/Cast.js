4ba59150326d95861fe1a6b893b232b7ae81c01c
Oleksandr <superbowl@ukr.net>
2022-10-19 21:13:55 +0300

    Update Cast.js

--- src/components/Cast/Cast.js
+++ src/components/Cast/Cast.js
@@ -22,26 +22,33 @@ const Cast = () => {
   if (!cast) {
     return null;
   }
+  if (cast.length === 0) {
+    return (
+      <div>
+        Sorry, but we have no information about the actors of this movie.
+      </div>
+    );
+  } else {
+    return (
+      <>
+        {cast.map(({ id, original_name, character, profile_path }) => (
+          <div key={id}>
+            {profile_path ? (
+              <img
+                src={`https://www.themoviedb.org/t/p/w200${profile_path}`}
+                alt={original_name}
+              />
+            ) : (
+              <IoIosPerson size={200} color="#6b6969" />
+            )}
 
-  return (
-    <>
-      {cast.map(({ id, original_name, character, profile_path }) => (
-        <div key={id}>
-          {profile_path ? (
-            <img
-              src={`https://www.themoviedb.org/t/p/w200${profile_path}`}
-              alt={original_name}
-            />
-          ) : (
-            <IoIosPerson size={200} color="#6b6969" />
-          )}
-
-          <div>{original_name}</div>
-          <div>Character: {character}</div>
-        </div>
-      ))}
-    </>
-  );
+            <div>{original_name}</div>
+            <div>Character: {character}</div>
+          </div>
+        ))}
+      </>
+    );
+  }
 };
 
 export default Cast;
