436fef5486c8b83e907c7ea3113ad6aca4988a7a
Oleksandr <superbowl@ukr.net>
2022-10-19 22:32:30 +0300

    Rename foulder Services

--- src/components/Reviews/Reviews.js
+++ src/components/Reviews/Reviews.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-import { fetchReviews } from '../../services/fetchMovies';
+import { fetchReviews } from '../../Services/fetchMovies';
 
 const Reviews = () => {
   const { movieId } = useParams();
