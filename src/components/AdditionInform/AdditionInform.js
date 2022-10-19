00739f108eca039976971c191232365a1ef47e64
Oleksandr <superbowl@ukr.net>
2022-10-19 23:20:52 +0300

    Update AdditionInform.js

--- src/components/AdditionInform/AdditionInform.js
+++ src/components/AdditionInform/AdditionInform.js
@@ -1,16 +1,23 @@
-import { NavLink, Outlet } from 'react-router-dom';
+import { NavLink, Outlet, useLocation } from 'react-router-dom';
 import { Suspense } from 'react';
 
 const AdditionInform = ({ movie: { id } }) => {
+  const location = useLocation();
+  const backLinkHref = location.state?.from ?? '/';
+
   return (
     <>
       <h3>Additional information</h3>
       <ul>
         <li>
-          <NavLink to="cast">Cast</NavLink>
+          <NavLink to="cast" state={{ from: backLinkHref }}>
+            Cast
+          </NavLink>
         </li>
         <li>
-          <NavLink to="reviews">Reviews</NavLink>
+          <NavLink to="reviews" state={{ from: backLinkHref }}>
+            Reviews
+          </NavLink>
         </li>
       </ul>
       <Suspense fallback={<div>Loading subpage...</div>}>
