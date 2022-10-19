26d0a3621669637be155bb8df46117182cf5a770
Oleksandr <superbowl@ukr.net>
2022-10-19 21:17:49 +0300

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,11 @@
 import { Routes, Route } from 'react-router-dom';
 import { lazy } from 'react';
 import SharedLayout from 'components/SharedLayout';
+import MovieDetails from 'pages/MovieDetails';
 
 const Home = lazy(() => import('../../pages/Home'));
 const Movies = lazy(() => import('../../pages/Movies'));
-const MovieDetails = lazy(() => import('../../pages/MovieDetails'));
+// const MovieDetails = lazy(() => import('../../pages/MovieDetails'));
 const Cast = lazy(() => import('../Cast'));
 const Reviews = lazy(() => import('../Reviews'));
 
