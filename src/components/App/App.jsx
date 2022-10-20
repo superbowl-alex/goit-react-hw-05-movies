28c600b3e6ba2a3edbab0f02a81659dbc5a58e58
Oleksandr <superbowl@ukr.net>
2022-10-20 10:17:32 +0300

    Rename Pages

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,9 +2,9 @@ import { Routes, Route } from 'react-router-dom';
 import React from 'react';
 import SharedLayout from 'components/SharedLayout';
 
-const Home = React.lazy(() => import('../../Pages/Home'));
-const Movies = React.lazy(() => import('../../Pages/Movies'));
-const MovieDetails = React.lazy(() => import('../../Pages/MovieDetails'));
+const Home = React.lazy(() => import('../../Stor/Home'));
+const Movies = React.lazy(() => import('../../Stor/Movies'));
+const MovieDetails = React.lazy(() => import('../../Stor/MovieDetails'));
 const Cast = React.lazy(() => import('../Cast'));
 const Reviews = React.lazy(() => import('../Reviews'));
 
